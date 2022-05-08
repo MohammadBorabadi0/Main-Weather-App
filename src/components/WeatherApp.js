0065291b4d12e9db30ed7d5d16b3774e28e1a6b7
mohammadborabadi0 <mohammadborabadi0@gmail.com>
2022-05-07 23:44:15 -0700

    responsive project

--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -6,7 +6,7 @@ import RightAside from "./RightAside";
 
 const WeatherApp = () => {
   return (
-    <main className="grid grid-cols-12 min-h-screen p-4 gap-2 bg-slate-900 max-w-7xl mx-auto md:mt-4">
+    <main className="grid grid-cols-12 min-h-screen p-1 sm:p-4 gap-2 bg-slate-900 max-w-7xl mx-auto md:mt-4">
       <section className="relative flex flex-col items-center rounded-md p-4 py-10 md:px-1 col-span-12 md:col-span-6 lg:col-span-4 bg-slate-800 shadow-md">
         <LeftAside />
       </section>
--- src/components/WeatherForm.js
+++ src/components/WeatherForm.js
@@ -16,7 +16,7 @@ const WeatherForm = ({
   return (
     <form
       onSubmit={handleSubmit}
-      className="flex flex-col justify-center items-center w-[90%] xl:w-[80%]"
+      className="flex flex-col justify-center items-center w-full sm:w-[90%] xl:w-[80%]"
     >
       <span
         className="absolute top-1.5 right-1.5 text-white text-xl"
@@ -33,11 +33,11 @@ const WeatherForm = ({
           onChange={(e) => setCity(e.target.value)}
           placeholder="Enter City Name"
           ref={inputRef}
-          className="flex-1 focus:outline-none font-semibold text-sm md:text-base rounded-sm px-1 lg:px-4 py-1.5"
+          className="flex-1 focus:outline-none font-semibold text-sm sm:text-md xl:text-base rounded-sm px-1 lg:px-4 py-1.5"
         />
         <button
           type="submit"
-          className="bg-blue-200 text-sm sm:text-base font-semibold rounded-sm px-1 lg:px-4 py-1.5 ml-1"
+          className="bg-blue-200 text-sm sm:text-md xl:text-base font-semibold rounded-sm px-1 lg:px-4 py-1.5 ml-1"
         >
           Search
         </button>
